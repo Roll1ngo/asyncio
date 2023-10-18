f371c98fb26ea964d945ef3d347f4c383c17449e
Vladislav B <131883552+Roll1ngo@users.noreply.github.com>
2023-10-18 13:45:50 +0300

    18.10.15_00

--- asyncio/forever_loop.py
+++ asyncio/forever_loop.py
@@ -1,9 +1,11 @@
 from time import time
 import asyncio
 from faker import Faker
+from concurrent.futures import ProcessPoolExecutor
 
 
 fake = Faker("uk-UA")
+url = "www.some_url.com"
 
 
 # async def get_user_asyncfrom_db(uuid: int):
@@ -14,10 +16,30 @@ fake = Faker("uk-UA")
 #         "company": fake.company(),
 #         "e-mail": fake.email(),
 #     }
+def cpu_bound_task(num):
+    counter = num
+    while num>0:
+        num-=1
+    print(f"Completed cpu_bound_task with{counter}")
+    return f"Completed cpu_bound_task with{counter}"
 
 
 async def send_metrics(url):
-    print(f"Send to {url}:{time()}")
+    while True:
+        await asyncio.sleep(1)
+        print(f"Send to {url}:{time()}")
+
+async def worker():
+    loop  = asyncio.get_running_loop()
+    sd = loop.create_task(send_metrics(url))
+
+
+    with ProcessPoolExecutor(4) as pool:
+        futures = [loop.run_in_executor(pool, cpu_bound_task,num) for num in [60_000_000, 70_000_000, 80_000_000]]
+        result = await asyncio.gather(*futures)
+        sd.cancel()
+        return result
+
 
     # async def main():
     #     u1_task = asyncio.create_task(get_user_asyncfrom_db(1))
@@ -26,11 +48,8 @@ async def send_metrics(url):
     #     print(u1_task, u2_task, u3_task)
 
 
-async def worker():
-    while True:
-        await asyncio.sleep(1)
-        await send_metrics("http://any_url.com")
 
+    
     # result = await asyncio.gather(u1_task, u2_task, u3_task)
     # return result
 
@@ -49,4 +68,5 @@ if __name__ == "__main__":
     #         json.dump(users, fd, indent=4, ensure_ascii=False)
     #     print(res)
     # print(time() - start)
-    asyncio.run(worker())
+    r=asyncio.run(worker())
+    print(r)
